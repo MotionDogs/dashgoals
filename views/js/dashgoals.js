6bc883e1e62f3a6b3c773b1b9ce3566bdce901f4
LoneWolf367 <lonewolf367@gmail.com>
2016-01-22 11:58:42 -0800

    Updated Tooltips JavaScript
    JavaScript updated to support NVD3 tooltips. Method used in previous
    versions has been depreciated.

--- views/js/dashgoals.js
+++ views/js/dashgoals.js
@@ -1,96 +1,105 @@
 var dashgoals_data;
 var dashgoals_chart;
 
-function bar_chart_goals(widget_name, chart_details)
-{
-	nv.addGraph(function() {
-		dashgoals_data = chart_details.data;
-
-		var chart = nv.models.multiBarChart()
-			.stacked(true)
-			.showControls(false)
-			.tooltipContent(function(key, y, e, graph) {
-				if (graph.value == 0)
-					return '';
+function bar_chart_goals(widget_name, chart_details){
+	
+	dashgoals_data = chart_details.data;
+	
+	//console.log(chart_details);	
+	
+    nv.addGraph(function() {
+            var chart = nv.models.multiBarChart()
+            	.showControls(false)
+                .stacked(true);
+                
+            var contentGenerator = chart.tooltip.contentGenerator();
+            var tooltip = chart.tooltip;
+            tooltip.contentGenerator(function(dashgoals_data){     
+	                
+	            key = dashgoals_data.data.key;
 
 				if (key == 'sales_real') {
-					var result = '<div class="tooltip-panel"><div class="tooltip-panel-heading">' + graph.series.title + '</div><strong>' + formatCurrency(parseInt(graph.point.sales), currency_format, currency_sign, currency_blank) + '</strong><br />(' + formatCurrency(parseInt(graph.point.goal), currency_format, currency_sign, currency_blank) + ')<br/>';
-					if (graph.point.sales > graph.point.goal)
+					var result = '<div class="tooltip-panel"><div class="tooltip-panel-heading">' + dashgoals_data.value + '</div><strong>' + formatCurrency(parseInt(dashgoals_data.data.sales), currency_format, currency_sign, currency_blank) + '</strong><br />(' + formatCurrency(parseInt(dashgoals_data.data.goal), currency_format, currency_sign, currency_blank) + ')<br/>';
+					if (dashgoals_data.data.sales > dashgoals_data.data.goal)
 						result += '<span class="dash_trend dash_trend_up">+';
 					else
 						result += '<span class="dash_trend dash_trend_down">';
-					result += graph.point.goal_diff + '%</span></div>';
+					result += dashgoals_data.data.goal_diff + '%</span></div>';
 					return result;
 				} else if (key == 'sales_less') {
-					if (graph.point.sales > 0)
-						return '<div class="tooltip-panel"><div class="tooltip-panel-heading">' + graph.series.title + '</div><strong>' + graph.series.zone_text + '</strong><br /><span class="dash_trend dash_trend_down">' + formatCurrency(parseInt(graph.point.goal_diff), currency_format, currency_sign, currency_blank) + '</span></div>';
+					if (dashgoals_data.data.sales > 0)
+						return '<div class="tooltip-panel"><div class="tooltip-panel-heading">' + dashgoals_data.value + '</div><strong>' + chart_details.data[dashgoals_data.data.series].zone_text + '</strong><br /><span class="dash_trend dash_trend_down">' + formatCurrency(parseInt(dashgoals_data.data.goal_diff), currency_format, currency_sign, currency_blank) + '</span></div>';
 					else
-						return '<div class="tooltip-panel"><div class="tooltip-panel-heading">' + graph.series.title + '</div><strong>' + graph.series.zone_text + '</strong><br />(' + formatCurrency(parseInt(graph.point.goal), currency_format, currency_sign, currency_blank) + ')</div>';
+						return '<div class="tooltip-panel"><div class="tooltip-panel-heading">' + dashgoals_data.value + '</div><strong>' + chart_details.data[dashgoals_data.data.series].zone_text + '</strong><br />(' + formatCurrency(parseInt(dashgoals_data.data.goal), currency_format, currency_sign, currency_blank) + ')</div>';
 				} else if (key == 'sales_more')
-					return '<div class="tooltip-panel"><div class="tooltip-panel-heading">' + graph.series.title + '</div><strong>' + graph.series.zone_text + '</strong><br /><span class="dash_trend dash_trend_up">+' + formatCurrency(parseInt(graph.point.goal_diff), currency_format, currency_sign, currency_blank) + '</span></div>';
+					return '<div class="tooltip-panel"><div class="tooltip-panel-heading">' + dashgoals_data.value + '</div><strong>' + chart_details.data[dashgoals_data.data.series].zone_text + '</strong><br /><span class="dash_trend dash_trend_up">+' + formatCurrency(parseInt(dashgoals_data.data.goal_diff), currency_format, currency_sign, currency_blank) + '</span></div>';
 				else if (key == 'avg_cart_value_real') {
-					var result = '<div class="tooltip-panel"><div class="tooltip-panel-heading">' + graph.series.title + '</div><strong>' + formatCurrency(parseInt(graph.point.avg_cart_value), currency_format, currency_sign, currency_blank) + '</strong><br />(' + formatCurrency(parseInt(graph.point.goal), currency_format, currency_sign, currency_blank) + ')<br/>';
-					if (graph.point.avg_cart_value > graph.point.goal)
+					var result = '<div class="tooltip-panel"><div class="tooltip-panel-heading">' + dashgoals_data.value + '</div><strong>' + formatCurrency(parseInt(dashgoals_data.data.avg_cart_value), currency_format, currency_sign, currency_blank) + '</strong><br />(' + formatCurrency(parseInt(dashgoals_data.data.goal), currency_format, currency_sign, currency_blank) + ')<br/>';
+					if (dashgoals_data.data.avg_cart_value > dashgoals_data.data.goal)
 						result += '<span class="dash_trend dash_trend_up">+';
 					else
 						result += '<span class="dash_trend dash_trend_down">';
-					result += graph.point.goal_diff + '%</span></div>';
+					result += dashgoals_data.data.goal_diff + '%</span></div>';
 					return result;
 				} else if (key == 'avg_cart_value_less') {
-					if (graph.point.avg_cart_value > 0)
-						return '<div class="tooltip-panel"><div class="tooltip-panel-heading">' + graph.series.title + '</div><strong>' + graph.series.zone_text + '</strong><br /><span class="dash_trend dash_trend_down">' + formatCurrency(parseInt(graph.point.goal_diff), currency_format, currency_sign, currency_blank) + '</span></div>';
+					if (dashgoals_data.data.avg_cart_value > 0)
+						return '<div class="tooltip-panel"><div class="tooltip-panel-heading">' + dashgoals_data.value + '</div><strong>' + chart_details.data[dashgoals_data.data.series].zone_text + '</strong><br /><span class="dash_trend dash_trend_down">' + formatCurrency(parseInt(dashgoals_data.data.goal_diff), currency_format, currency_sign, currency_blank) + '</span></div>';
 					else
-						return '<div class="tooltip-panel"><div class="tooltip-panel-heading">' + graph.series.title + '</div><strong>' + graph.series.zone_text + '</strong><br />(' + formatCurrency(parseInt(graph.point.goal), currency_format, currency_sign, currency_blank) + ')</div>';
+						return '<div class="tooltip-panel"><div class="tooltip-panel-heading">' + dashgoals_data.value + '</div><strong>' + chart_details.data[dashgoals_data.data.series].zone_text + '</strong><br />(' + formatCurrency(parseInt(dashgoals_data.data.goal), currency_format, currency_sign, currency_blank) + ')</div>';
 				} else if (key == 'avg_cart_value_more')
-					return '<div class="tooltip-panel"><div class="tooltip-panel-heading">' + graph.series.title + '</div><strong>' + graph.series.zone_text + '</strong><br /><span class="dash_trend dash_trend_up">+' + formatCurrency(parseInt(graph.point.goal_diff), currency_format, currency_sign, currency_blank) + '</span></div>';
+					return '<div class="tooltip-panel"><div class="tooltip-panel-heading">' + dashgoals_data.value + '</div><strong>' + chart_details.data[dashgoals_data.data.series].zone_text + '</strong><br /><span class="dash_trend dash_trend_up">+' + formatCurrency(parseInt(dashgoals_data.data.goal_diff), currency_format, currency_sign, currency_blank) + '</span></div>';
 				else if (key == 'traffic_real') {
-					var result = '<div class="tooltip-panel"><div class="tooltip-panel-heading">' + graph.series.title + '</div><strong>' + graph.point.traffic + ' ' +graph.series.unit_text+'</strong><br />(' + graph.point.goal + ' ' +graph.series.unit_text+')<br/>';
-					if (graph.point.traffic > graph.point.goal)
+					var result = '<div class="tooltip-panel"><div class="tooltip-panel-heading">' + dashgoals_data.value + '</div><strong>' + dashgoals_data.data.traffic + ' ' +chart_details.data[dashgoals_data.index].unit_text+'</strong><br />(' + dashgoals_data.data.goal + ' ' +chart_details.data[dashgoals_data.data.series].unit_text+')<br/>';
+					if (dashgoals_data.data.traffic > dashgoals_data.data.goal)
 						result += '<span class="dash_trend dash_trend_up">+';
 					else
 						result += '<span class="dash_trend dash_trend_down">';
-					result += graph.point.goal_diff + '%</span></div>';
+					result += dashgoals_data.data.goal_diff + '%</span></div>';
 					return result;
 				} else if (key == 'traffic_less') {
-					if (graph.point.traffic > 0)
-						return '<div class="tooltip-panel"><div class="tooltip-panel-heading">' + graph.series.title + '</div><strong>' + graph.series.zone_text + '</strong><br /><span class="dash_trend dash_trend_down">' + graph.point.goal_diff + ' ' +graph.series.unit_text+'</span></div>';
+					if (dashgoals_data.data.traffic > 0)
+						return '<div class="tooltip-panel"><div class="tooltip-panel-heading">' + dashgoals_data.value + '</div><strong>' + chart_details.data[dashgoals_data.data.series].zone_text + '</strong><br /><span class="dash_trend dash_trend_down">' + dashgoals_data.data.goal_diff + ' ' +chart_details.data[dashgoals_data.data.series].unit_text+'</span></div>';
 					else
-						return '<div class="tooltip-panel"><div class="tooltip-panel-heading">' + graph.series.title + '</div><strong>' + graph.series.zone_text + '</strong><br />(' + graph.point.goal + ' ' +graph.series.unit_text+')</div>';
+						return '<div class="tooltip-panel"><div class="tooltip-panel-heading">' + dashgoals_data.value + '</div><strong>' + chart_details.data[dashgoals_data.data.series].zone_text + '</strong><br />(' + dashgoals_data.data.goal + ' ' +chart_details.data[dashgoals_data.data.series].unit_text+')</div>';
 				} else if (key == 'traffic_more')
-					return '<div class="tooltip-panel"><div class="tooltip-panel-heading">' + graph.series.title + '</div><strong>' + graph.series.zone_text + '</strong><br /><span class="dash_trend dash_trend_up">+' + graph.point.goal_diff + ' ' +graph.series.unit_text+'</span></div>';
+					return '<div class="tooltip-panel"><div class="tooltip-panel-heading">' + dashgoals_data.value + '</div><strong>' + chart_details.data[dashgoals_data.index].zone_text + '</strong><br /><span class="dash_trend dash_trend_up">+' + dashgoals_data.data.goal_diff + ' ' +chart_details.data[dashgoals_data.index].unit_text+'</span></div>';
 				else if (key == 'conversion_real') {
-					var result = '<div class="tooltip-panel"><div class="tooltip-panel-heading">' + graph.series.title + '</div><strong>' + graph.point.conversion + '%</strong><br />(' + graph.point.goal + '%)</strong><br/>';
-					if (graph.point.conversion  > graph.point.goal)
+					var result = '<div class="tooltip-panel"><div class="tooltip-panel-heading">' + dashgoals_data.value + '</div><strong>' + dashgoals_data.data.conversion + '%</strong><br />(' + dashgoals_data.data.goal + '%)</strong><br/>';
+					if (dashgoals_data.data.conversion  > dashgoals_data.data.goal)
 						result += '<span class="dash_trend dash_trend_up">+';
 					else
 						result += '<span class="dash_trend dash_trend_down">';
-					result += graph.point.goal_diff + '%</span></div>';
+					result += dashgoals_data.data.goal_diff + '%</span></div>';
 					return result;
 				} else if (key == 'conversion_less') {
-					if (graph.point.conversion > 0)
-						return '<div class="tooltip-panel"><div class="tooltip-panel-heading">' + graph.series.title + '</div><strong>' + graph.series.zone_text + '</strong><br /><span class="dash_trend dash_trend_down">' + graph.point.goal_diff + '%</span></div>';
+					if (dashgoals_data.data.conversion > 0)
+						return '<div class="tooltip-panel"><div class="tooltip-panel-heading">' + dashgoals_data.value + '</div><strong>' + chart_details.data[dashgoals_data.data.series].zone_text + '</strong><br /><span class="dash_trend dash_trend_down">' + dashgoals_data.data.goal_diff + '%</span></div>';
 					else
-						return '<div class="tooltip-panel"><div class="tooltip-panel-heading">' + graph.series.title + '</div><strong>' + graph.series.zone_text + '</strong><br />(' + graph.point.goal + '%)</div>';
-				} else if (key == 'conversion_more')
-					return '<div class="tooltip-panel"><div class="tooltip-panel-heading">' + graph.series.title + '</div><strong>' + graph.series.zone_text + '</strong><br /><span class="dash_trend dash_trend_up">+' + graph.point.goal_diff + '%</span></div>';
+						return '<div class="tooltip-panel"><div class="tooltip-panel-heading">' + chart_details.data[dashgoals_data.index].title + '</div><strong>' + chart_details.data[dashgoals_data.data.series].zone_text + '</strong><br />(' + dashgoals_data.data.goal + '%)</div>';
+				} else if (key == 'conversion_more'){
+					return '<div class="tooltip-panel"><div class="tooltip-panel-heading">' + dashgoals_data.value + '</div><strong>' + chart_details.data[dashgoals_data.data.series].zone_text + '</strong><br /><span class="dash_trend dash_trend_up">+' + dashgoals_data.data.goal_diff + '%</span></div>';
+				}
+	           //return contentGenerator(chart_details.series.key);
+	        });
 
-			});
+			chart.yAxis.tickFormat(d3.format('.1f'));
+			dashgoals_chart = chart;
+	
+			d3.select('#dash_goals_chart1 svg')
+				.datum(chart_details.data)
+				.transition()
+				.call(chart);
+	
+			$('#dash_goals_chart1 .nv-legendWrap').remove();
+	
+			nv.utils.windowResize(chart.update);
 
-		chart.yAxis.tickFormat(d3.format('.1f'));
-		dashgoals_chart = chart;
-
-		d3.select('#dash_goals_chart1 svg')
-			.datum(chart_details.data)
-			.transition()
-			.call(chart);
-
-		$('#dash_goals_chart1 .nv-legendWrap').remove();
-
-		nv.utils.windowResize(chart.update);
-
-		return chart;
-	});
+            return chart;
+    });
 }
+
+
+
 
 function selectDashgoalsChart(type)
 {
